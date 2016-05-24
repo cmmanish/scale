cef04642e7c68201e3d214e5932059579076c708
Manish Madhusoodan <manish@shopkick.com>
2016-05-23 23:19:39 -0700

    Face detection

---
+++ app/src/main/java/com/android/scale/FaceDetectActivity.java
@@ -0,0 +1,39 @@
+package com.android.scale;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by mmadhusoodan on 5/23/16.
+ */
+public class FaceDetectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+
+        });
+
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = true;
+        Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.test1, options);
+
+    }
+}
--- app/src/main/java/com/android/scale/MainActivity.java
+++ app/src/main/java/com/android/scale/MainActivity.java
@@ -5,18 +5,23 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
+import android.graphics.*;
 import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.android.scale.Backend.DatabaseHandler;
 import com.android.scale.Backend.Instagram4J;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -28,14 +33,12 @@ public class MainActivity extends Activity {
     private TextView textView1, textView2;
     private SQLiteDatabase db;
     private String str = "VIRAT";
-    Instagram4J instagram4J = new Instagram4J();
-    ArrayList<Bitmap> imageList = null;
-
+    private Instagram4J instagram4J = new Instagram4J();
+    private ArrayList<Bitmap> imageList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
@@ -43,22 +46,50 @@ public class MainActivity extends Activity {
         db = this.openOrCreateDatabase("image.db", Context.MODE_PRIVATE, null);
         DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
         //Log.i(TAG, "Database Dropped " + databaseHandler.dropTable(db));
-
         Log.i(TAG, "Database " + databaseHandler.checkDataBase());
 
-        imageView1 = (ImageView) findViewById(R.id.imageView1);
         textView1 = (TextView) findViewById(R.id.textView1);
-
         imageView2 = (ImageView) findViewById(R.id.imageView2);
         textView2 = (TextView) findViewById(R.id.textView2);
     }
 
-    public void downloadSaveAndDisplayImage(View view) {
+    public void detectFaceInTheImage(View view) {
         try {
             imageList = instagram4J.getBitmapsFromTagSearch(str);
             Log.i(TAG, "Row Number " + insertImage(imageList.get(2)));
-            Toast.makeText(this, "Insert Success", Toast.LENGTH_SHORT).show();
-            imageView1.setImageBitmap(imageList.get(2));
+            Bitmap myBitmap = imageList.get(2);
+
+            imageView1 = (ImageView) findViewById(R.id.imageView1);
+            imageView1.setImageBitmap(myBitmap);
+
+            Paint myRectPaint = new Paint();
+            myRectPaint.setStrokeWidth(5);
+            myRectPaint.setColor(Color.RED);
+            myRectPaint.setStyle(Paint.Style.STROKE);
+
+            Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+            Canvas tempCanvas = new Canvas(tempBitmap);
+            tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+            FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false).build();
+            if (!faceDetector.isOperational()) {
+                new AlertDialog.Builder(getApplicationContext()).setMessage("Could not set up the face detector!").show();
+                return;
+            }
+
+            Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+            SparseArray<Face> faces = faceDetector.detect(frame);
+
+            for (int i = 0; i < faces.size(); i++) {
+                Face thisFace = faces.valueAt(i);
+                float x1 = thisFace.getPosition().x;
+                float y1 = thisFace.getPosition().y;
+                float x2 = x1 + thisFace.getWidth();
+                float y2 = y1 + thisFace.getHeight();
+                tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+            }
+            imageView1.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+
             db.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -75,18 +106,18 @@ public class MainActivity extends Activity {
                 imageView2.setImageBitmap(bmp);
                 Toast.makeText(this, "DISPLAY Success", Toast.LENGTH_SHORT).show();
             }
-            db.close();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            db.close();
         }
     }
 
     public long insertImage(Bitmap img) {
-
-        byte[] data = getBitmapAsByteArray(img);
-        ContentValues values = new ContentValues();
-        values.put("image", data);
         try {
+            byte[] data = getBitmapAsByteArray(img);
+            ContentValues values = new ContentValues();
+            values.put("image", data);
             db = openOrCreateDatabase("image.db", Context.MODE_PRIVATE, null);
             long rowid = db.insert("image_table", null, values);
             if (rowid == -1) {
@@ -104,8 +135,13 @@ public class MainActivity extends Activity {
     }
 
     public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        bitmap.compress(CompressFormat.PNG, 0, outputStream);
-        return outputStream.toByteArray();
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(CompressFormat.PNG, 0, outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
